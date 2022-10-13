b71f7e8f97a81a202d122e476422ee04b218b677
Yaotian <yawu@ucsd.edu>
2022-10-13 16:56:47 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -4,28 +4,29 @@ import java.net.URI;
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
-    String[] ram = new String[]{ "Start" };
+    String ram = "Start";
+    String after = "";
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) { //check if the path is /.
-            return String.format("String: %d", ram);//shows the value of ram
+            return String.format("String: "+ ram);//shows the value of ram
         } else {//in the other cases
             System.out.println("Path: " + url.getPath());//shows Path: the path we have.
             if (url.getPath().contains("/add")) {//check if the path contains /add
                 String[] parameters = url.getQuery().split("=");//split the query at =.
                 if (parameters[0].equals("s")) {//check if stuff at the beginning of query is s
-                    ram.add(parameters[1]);//add the String in the query to ram.
-                    return String.format("String added:%s! It's now %d", parameters[1], ram);
+                    ram = ram + " " + parameters[1];//add the String in the query to ram.
+                    return String.format("String added:" + parameters[1] + "! It's now " + ram);
                 }
             }else if (url.getPath().contains("/search")){
                 String[] parameters = url.getQuery().split("=");
                 if (parameters[0].equals("s")) {
-                    String[] check = new String[]{};
-                    for(int i = 0; i < ram.length ; i++) {
-                        if (ram[i].equals(parameters[1])) {
-                            check.add(ram[i]);
+                    String check[] = ram.split(" ");
+                    for(int i = 1; i < check.length ; i++) {
+                        if (check[i].contains(parameters[1])) {
+                            after = after + check[i] + " ";
                         }
                     }
-                    return String.format("In the String:%s contains %d", check, parameters[1]);
+                    return String.format("In the String:" + after + "contains " + parameters[1]);
                 }
             }
             return "404 Not Found!";
@@ -33,7 +34,7 @@ class Handler implements URLHandler {
     }
 }
 
-class NumberServer {
+class SearchEngine {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
